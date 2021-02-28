714737d72bb8c8c35f2810ba04686b663d6ca023
Mk <furtermm@mail.uc.edu>
2021-02-28 18:28:17 -0500

    Implemented ICountryDAO, CountryService classes and RetrofitClientInstance

--- app/src/main/java/app/plantdiary/individualassignment3048q/dto/Country.kt
+++ app/src/main/java/app/plantdiary/individualassignment3048q/dto/Country.kt
@@ -1,6 +1,13 @@
 package app.plantdiary.individualassignment3048q.dto
 
-class Country (var code: String, var name: String){
+import com.google.gson.annotations.SerializedName
+
+/**
+ * Added properties to Country class to make the test pass
+ * Used @SerializedName to match the JSON exactly
+ */
+class Country (@SerializedName("Code") var code: String, @SerializedName("Name") var name: String){
+
     override fun toString(): String {
         return name + " " + code
     }
--- app/src/main/java/app/plantdiary/individualassignment3048q/ui/main/MainViewModel.kt
+++ app/src/main/java/app/plantdiary/individualassignment3048q/ui/main/MainViewModel.kt
@@ -3,11 +3,18 @@ package app.plantdiary.individualassignment3048q.ui.main
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import app.plantdiary.individualassignment3048q.dto.Country
+import app.plantdiary.individualassignment3048q.service.CountryService
+import retrofit2.Call
+import retrofit2.http.GET
 
+/**
+ * Created ICountryDAO that contains GET Function
+ */
 class MainViewModel : ViewModel() {
     var countries: MutableLiveData<ArrayList<Country>> = MutableLiveData<ArrayList<Country>>()
+    var countryService: CountryService = CountryService()
 
     fun fetchCountries() {
-
+        countries = countryService.fetchCountries()
     }
 }
