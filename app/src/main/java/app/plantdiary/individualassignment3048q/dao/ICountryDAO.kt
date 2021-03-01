bf72b0c1722d1ae553a5f112b1fdd9b11d23d3a4
Mk <furtermm@mail.uc.edu>
2021-02-28 19:06:51 -0500

    Added comments in DAO

--- app/src/main/java/app/plantdiary/individualassignment3048q/dao/ICountryDAO.kt
+++ app/src/main/java/app/plantdiary/individualassignment3048q/dao/ICountryDAO.kt
@@ -1,3 +1,9 @@
+/**
+ * Interface use for Retrofit in CountryService.kt
+ *
+ * @Get domain is defined in RetrofitClientInstance.kt
+ * The Full URL for reference: https://pkgstore.datahub.io/core/country-list/data_json/data/8c458f2d15d9f2119654b29ede6e45b8/data_json.json
+ */
 package app.plantdiary.individualassignment3048q.dao
 
 import app.plantdiary.individualassignment3048q.dto.Country
