380a2db228e3a2143cdfda99c01c2a624e50584b
Mk <furtermm@mail.uc.edu>
2021-02-28 19:34:03 -0500

    Added comments in Class dto.

--- app/src/main/java/app/plantdiary/individualassignment3048q/dto/Country.kt
+++ app/src/main/java/app/plantdiary/individualassignment3048q/dto/Country.kt
@@ -1,11 +1,11 @@
+/**
+ * Added properties to Country class to make the test pass
+ * Used @SerializedName to match the JSON exactly with capital letters (Code and Name)
+ */
 package app.plantdiary.individualassignment3048q.dto
 
 import com.google.gson.annotations.SerializedName
 
-/**
- * Added properties to Country class to make the test pass
- * Used @SerializedName to match the JSON exactly
- */
 class Country (@SerializedName("Code") var code: String, @SerializedName("Name") var name: String){
 
     override fun toString(): String {
